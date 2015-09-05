b0c852aaa709383ce8adf80336127e4d43f00174
lycha611 <luczak.bartosz5@gmail.com>
2015-09-05 21:10:38 +0100

    add corelib and sharedresources modules to project

--- Android/CookMe/app/src/main/java/com/spoon/cookme/HomeActivity.java
+++ Android/CookMe/app/src/main/java/com/spoon/cookme/HomeActivity.java
@@ -10,8 +10,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+        setContentView(R.layout.activity_home);    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
